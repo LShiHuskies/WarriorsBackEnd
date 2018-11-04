6f5b443c611d11c2b2671fc6292406ab0bf344c2
github username <github email address>
2018-11-04 14:49:53 -0500

    environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_cable.url = "wss://warriorbackend.herokuapp.com/cable"
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
